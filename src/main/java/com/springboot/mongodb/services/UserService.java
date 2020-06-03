5536bb7aa42f0eaec862abfab8366536bc200e79
Rodrigo Pereira Marques_ <rodrigop@jv01.local>
2020-06-03 18:49:52 -0300

    Recuperando usuário por id

--- src/main/java/com/springboot/mongodb/services/UserService.java
+++ src/main/java/com/springboot/mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.springboot.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.mongodb.domain.User;
 import com.springboot.mongodb.repository.UserRepository;
+import com.springboot.mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
     private UserRepository userRepository;
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+         Optional<User> user = userRepository.findById(id);
+         return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
 }

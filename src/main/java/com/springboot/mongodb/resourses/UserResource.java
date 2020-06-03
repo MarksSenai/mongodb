e615e555f768cc8f37c88e97a2a1014b57840326
Rodrigo Pereira Marques_ <rodrigop@jv01.local>
2020-06-03 18:08:51 -0300

    Inclusão do padrão DTO

--- src/main/java/com/springboot/mongodb/resourses/UserResource.java
+++ src/main/java/com/springboot/mongodb/resourses/UserResource.java
@@ -1,8 +1,7 @@
 package com.springboot.mongodb.resourses;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.springboot.mongodb.domain.User;
+import com.springboot.mongodb.dto.UserDTO;
 import com.springboot.mongodb.services.UserService;
 
 @RestController
@@ -21,7 +20,9 @@ public class UserResource {
     private UserService userService;
 
     @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok().body(userService.findAll());
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<UserDTO> userDTOList = userService.findAll().stream()
+                .map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOList);
     }
 }

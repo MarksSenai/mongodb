e7742b167a85b02ff2c1ca27f9c172d51d736e23
Rodrigo Pereira Marques_ <rodrigop@jv01.local>
2020-06-03 17:53:49 -0300

    Instanciamento da base de dados

---
+++ src/main/java/com/springboot/mongodb/services/UserService.java
@@ -0,0 +1,19 @@
+package com.springboot.mongodb.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.mongodb.domain.User;
+import com.springboot.mongodb.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
